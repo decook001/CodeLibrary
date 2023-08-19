57eeef9e150f11863a6721714c81c4cf5ced09fc
Shoaib Tasrif <107314630+shoaib221@users.noreply.github.com>
2023-08-19 21:06:50 +0600

    Update and rename diophantine_eqn.txt to diophantine_eqn.cpp

--- maths/diophantine_eqn.cpp
+++ maths/diophantine_eqn.cpp
@@ -1,4 +1,4 @@
-
+/*
 Theory
   
   for given (a,b,c), ax+by=c has a solution for some (x,y)  <->  gcd(a,b) | c 
@@ -23,3 +23,31 @@ Solutions :
 if a solution (x,y) exists then there exists an infinite number of solutions
 x'=x+k*lcm(a,b)
 y'=y-k*lcm(a,b)
+*/
+
+int exuclid( int a, int b, int& x, int& y )
+{
+    // ret=ax+by
+    if(b==0)
+    {
+        x=1;
+        y=0;
+        return a;
+    }
+    int g=exuclid( b, a%b, x, y );
+    int tmp=y;
+    y=x-(a/b)*y;
+    x=tmp;
+    return g;   //   g=ax+by
+}
+
+
+int diophantine( int a, int& x, int b, int& y, int c )
+{
+    int g=exuclid( a,b , x, y );
+    if( c%g ) return 0;
+    c/=g;
+    x*=c;
+    y*=c;
+    return 1;
+}
