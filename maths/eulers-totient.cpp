6250271c19db78d7dcf44569cc099fc777e4e193
Shoaib Tasrif <107314630+shoaib221@users.noreply.github.com>
2023-08-19 21:16:28 +0600

    Update eulers-totient.cpp

--- maths/eulers-totient.cpp
+++ maths/eulers-totient.cpp
@@ -1,1 +1,20 @@
 
+
+struct Totient
+{
+    vi totient;
+    void build_totient(int n)
+    {
+        totient=vi (n+9);
+        iota( totient.begin(), totient.end(), 0 );
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=i*2; j<=n; j+=i)
+            {
+                totient[j]-=totient[i];
+            }
+        }
+    }
+
+    
+};
