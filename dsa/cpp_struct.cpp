1c4123d3b0f7482dff786fdc556ad26e2fa1cb65
Shoaib Tasrif <107314630+shoaib221@users.noreply.github.com>
2023-08-20 10:19:11 +0600

    Create cpp_struct.cpp

---
+++ dsa/cpp_struct.cpp
@@ -0,0 +1,81 @@
+
+struct Pos
+{
+
+    int x,y;
+
+    Pos()
+    {
+        x=0;
+        y=0;
+    }
+
+    Pos(int a, int b)
+    {
+        x=a;
+        y=b;
+    }
+
+    Pos(const Pos& a)
+    {
+        x=a.x;
+        y=a.y;
+    }
+
+    Pos(char c)
+    {
+        if(c=='L')x=-1,y=0;
+        if(c=='R')x=1,y=0;
+        if(c=='U')x=0,y=1;
+        if(c=='D')x=0,y=-1;
+    }
+
+    Pos operator + (Pos l)
+    {
+        Pos res;
+        res.x=x+l.x;
+        res.y=y+l.y;
+        return res;
+    }
+
+    Pos operator - (Pos a)
+    {
+        Pos res;
+        res.x=x-a.x;
+        res.y=y-a.y;
+        return res;
+    }
+
+    Pos operator += (Pos a)
+    {
+        x+=a.x;
+        y+=a.y;
+        return *this;
+    }
+
+    bool operator == (Pos a)
+    {
+        if(x==a.x && y==a.y)return true;
+        return false;
+    }
+
+    bool operator != (Pos a)
+    {
+        if(x!=a.x || y!=a.y )return true;
+        return false;
+    }
+
+    bool operator < (Pos dan )
+    {
+        if(x==dan.x)return y<dan.y;
+        return x<dan.x;
+    }
+
+    void print()
+    {
+        cout<<x<<S<<y<<N;
+    }
+
+};
+
+
