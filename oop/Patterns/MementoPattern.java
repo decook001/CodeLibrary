47b52be6d0acfad6d36dc78e086d575be19f0b28
Md Shoaib Tasrif Emon <107314630+shoaib221@users.noreply.github.com>
2023-09-25 12:59:07 +0600

    Update MementoPattern.java

--- oop/Patterns/MementoPattern.java
+++ oop/Patterns/MementoPattern.java
@@ -1,34 +1,7 @@
 // Capture and restore an object's internal state
 // tutorials point
 
-
-public class Main{
-    public static void main(String[] args) {
-        Caretaker caretaker = new Caretaker();
-        Originator originator = new Originator();
-        originator.setState("State1");
-        originator.setState("State2");
-        caretaker.addMemento( originator.save() );
-        originator.setState("State3");
-        caretaker.addMemento( originator.save() );
-        originator.setState("State4");
-        originator.restore( caretaker.getMemento() );
-    }
-}
-
-
-class Caretaker {
-    private ArrayList<Memento> mementos = new ArrayList<>();
-
-    public void addMemento(Memento m) {
-        mementos.add(m);
-    }
-
-    public Memento getMemento() {
-        return mementos.get(1);
-    }
-}
-
+import java.util.*;
 
 class Memento {
     private String state;
@@ -42,21 +15,59 @@ class Memento {
     }
 }
 
-class Originator {
+class Originator 
+{
     private String state;
+   
 
-
-    public void setState(String state) {
+    public void setState(String state) 
+    {
         System.out.println("Originator: Setting state to " + state);
         this.state = state;
     }
 
-    public Memento save() {
+    public Memento save() 
+    {
         System.out.println("Originator: Saving to Memento.");
         return new Memento(state);
     }
-    public void restore(Memento m) {
+    public void restore(Memento m) 
+    {
         state = m.getState();
         System.out.println("Originator: State after restoring from Memento: " + state);
     }
 }
+
+class Caretaker 
+{
+    private ArrayList<Memento> mementos = new ArrayList<>();
+    public void addMemento(Memento m) 
+    {
+        mementos.add(m);
+    }
+
+    public Memento getMemento() 
+    {
+        Memento ret=null;
+        if( mementos.size()>0 ) ret=mementos.get( mementos.size()-1  );
+        mementos.remove( mementos.size()-1  );
+        return ret;
+    }
+}
+
+public class MementoPattern 
+{
+    public static void main(String[] args) 
+    {
+        Caretaker caretaker = new Caretaker();
+        Originator originator = new Originator();
+        originator.setState("State1");
+        originator.setState("State2");
+        caretaker.addMemento( originator.save() );
+        originator.setState("State3");
+        caretaker.addMemento( originator.save() );
+        originator.setState("State4");
+        originator.restore( caretaker.getMemento() );
+    }
+}
+
