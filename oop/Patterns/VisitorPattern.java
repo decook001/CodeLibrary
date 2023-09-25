25b516a465dba97a96a64f9162377c8c43959894
Md Shoaib Tasrif Emon <107314630+shoaib221@users.noreply.github.com>
2023-09-25 17:04:27 +0600

    Update VisitorPattern.java

--- oop/Patterns/VisitorPattern.java
+++ oop/Patterns/VisitorPattern.java
@@ -1,178 +1,64 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package VisitorPattern;
+// https://www.tutorialspoint.com/design_pattern/visitor_pattern.html
 
-/**
- *
- * @author shoai
- */
-public class Circle extends Dot {
-    private int radius;
+interface ComputerPart 
+{
+   public void accept(ComputerPartVisitor computerPartVisitor);
+}
 
-    public Circle(int id, int x, int y, int radius) {
-        super(id, x, y);
-        this.radius = radius;
-    }
+class Keyboard implements ComputerPart 
+{
+   @Override
+   public void accept(ComputerPartVisitor computerPartVisitor) 
+   {
+      computerPartVisitor.visit(this);
+   }
+}
 
-    @Override
-    public String accept(Visitor visitor) {
-        return visitor.visitCircle(this);
-    }
+class Mouse implements ComputerPart 
+{
 
-    public int getRadius() {
-        return radius;
-    }
+   @Override
+   public void accept(ComputerPartVisitor computerPartVisitor) 
+   {
+      computerPartVisitor.visit(this);
+   }
+}
+
+interface ComputerPartVisitor 
+{
+
+	public void visit(Mouse mouse);
+	public void visit(Keyboard keyboard);
+
+}
+
+class ComputerPartDisplayVisitor implements ComputerPartVisitor 
+{
+
+   @Override
+   public void visit(Mouse mouse) {
+      System.out.println("Displaying Mouse.");
+   }
+
+   @Override
+   public void visit(Keyboard keyboard) {
+      System.out.println("Displaying Keyboard.");
+   }
+    
+}
+
+public class VisitorPattern
+{
+   public static void main(String[] args) 
+    {
+        ComputerPart cp = new Mouse();
+        ComputerPartVisitor dv = new ComputerPartDisplayVisitor();
+        cp.accept( dv );
+
+        cp = new Keyboard();
+        cp.accept( dv );
+   }
 }
 
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class CompoundShape implements Shape {
-    public int id;
-    public List<Shape> children = new ArrayList<>();
-
-    public CompoundShape(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public void move(int x, int y) {
-        // move shape
-    }
-
-    @Override
-    public void draw() {
-        // draw shape
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public String accept(Visitor visitor) {
-        return visitor.visitCompoundGraphic(this);
-    }
-
-    public void add(Shape shape) {
-        children.add(shape);
-    }
-}
-
-
-public class Dot implements Shape {
-    private int id;
-    private int x;
-    private int y;
-
-    public Dot() {
-    }
-
-    public Dot(int id, int x, int y) {
-        this.id = id;
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public void move(int x, int y) {
-        // move shape
-    }
-
-    @Override
-    public void draw() {
-        // draw shape
-    }
-
-    @Override
-    public String accept(Visitor visitor) {
-        return visitor.visitDot(this);
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getId() {
-        return id;
-    }
-}
-
-
-public class Rectangle implements Shape {
-    private int id;
-    private int x;
-    private int y;
-    private int width;
-    private int height;
-
-    public Rectangle(int id, int x, int y, int width, int height) {
-        this.id = id;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-
-    @Override
-    public String accept(Visitor visitor) {
-        return visitor.visitRectangle(this);
-    }
-
-    @Override
-    public void move(int x, int y) {
-        // move shape
-    }
-
-    @Override
-    public void draw() {
-        // draw shape
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-}
-
-
-public interface Shape {
-    void move(int x, int y);
-    void draw();
-    String accept(Visitor visitor);
-}
-
-
-
-public interface Visitor {
-    String visitDot(Dot dot);
-
-    String visitCircle(Circle circle);
-
-    String visitRectangle(Rectangle rectangle);
-
-    String visitCompoundGraphic(CompoundShape cg);
-}
 
