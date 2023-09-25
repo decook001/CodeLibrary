30e9c2853c3beb884baf35ecfa8c121d8b79bd3f
Md Shoaib Tasrif Emon <107314630+shoaib221@users.noreply.github.com>
2023-09-25 18:41:06 +0600

    Update StatePattern.java

--- oop/Patterns/StatePattern.java
+++ oop/Patterns/StatePattern.java
@@ -1,227 +1,73 @@
-
-public class Maven_java {
-    public static void main(String[] args) {
-        Player player = new Player();
-        UI ui = new UI(player);
-        ui.init();
-    }
+public interface State 
+{
+   public void doAction(Context context);
 }
 
-class LockedState extends State {
+public class StartState implements State 
+{
 
-    LockedState(Player player) {
-        super(player);
-        player.setPlaying(false);
-    }
+   public void doAction(Context context) 
+   {
+      System.out.println("Player is in start state");
+      context.setState(this);	
+   }
 
-    @Override
-    public String onLock() {
-        if (player.isPlaying()) {
-            player.changeState(new ReadyState(player));
-            return "Stop playing";
-        } else {
-            return "Locked...";
-        }
-    }
-
-    @Override
-    public String onPlay() {
-        player.changeState(new ReadyState(player));
-        return "Ready";
-    }
-
-    @Override
-    public String onNext() {
-        return "Locked...";
-    }
-
-    @Override
-    public String onPrevious() {
-        return "Locked...";
-    }
+   public String toString()
+   {
+      return "Start State";
+   }
 }
 
+public class StopState implements State 
+{
 
-import java.util.ArrayList;
-import java.util.List;
+   public void doAction(Context context) 
+    {
+      System.out.println("Player is in stop state");
+      context.setState(this);	
+   }
 
-public class Player {
-    private State state;
-    private boolean playing = false;
-    private List<String> playlist = new ArrayList<>();
-    private int currentTrack = 0;
-
-    public Player() {
-        this.state = new ReadyState(this);
-        setPlaying(true);
-        for (int i = 1; i <= 12; i++) {
-            playlist.add("Track " + i);
-        }
-    }
-
-    public void changeState(State state) {
-        this.state = state;
-    }
-
-    public State getState() {
-        return state;
-    }
-
-    public void setPlaying(boolean playing) {
-        this.playing = playing;
-    }
-
-    public boolean isPlaying() {
-        return playing;
-    }
-
-    public String startPlayback() {
-        return "Playing " + playlist.get(currentTrack);
-    }
-
-    public String nextTrack() {
-        currentTrack++;
-        if (currentTrack > playlist.size() - 1) {
-            currentTrack = 0;
-        }
-        return "Playing " + playlist.get(currentTrack);
-    }
-
-    public String previousTrack() {
-        currentTrack--;
-        if (currentTrack < 0) {
-            currentTrack = playlist.size() - 1;
-        }
-        return "Playing " + playlist.get(currentTrack);
-    }
-
-    public void setCurrentTrackAfterStop() {
-        this.currentTrack = 0;
-    }
+   public String toString()
+   {
+      return "Stop State";
+   }
 }
 
+public class Context 
+{
+   private State state;
 
-public class PlayingState extends State {
+   public Context()
+   {
+      state = null;
+   }
 
-    PlayingState(Player player) {
-        super(player);
-    }
+   public void setState(State state)
+   {
+      this.state = state;		
+   }
 
-    @Override
-    public String onLock() {
-        player.changeState(new LockedState(player));
-        player.setCurrentTrackAfterStop();
-        return "Stop playing";
-    }
-
-    @Override
-    public String onPlay() {
-        player.changeState(new ReadyState(player));
-        return "Paused...";
-    }
-
-    @Override
-    public String onNext() {
-        return player.nextTrack();
-    }
-
-    @Override
-    public String onPrevious() {
-        return player.previousTrack();
-    }
+   public State getState()
+   {
+      return state;
+   }
 }
 
+public class StatePattern 
+{
+   public static void main(String[] args) 
+   {
+      Context context = new Context();
 
-public class ReadyState extends State {
+      StartState startState = new StartState();
+      startState.doAction(context);
 
-    public ReadyState(Player player) {
-        super(player);
-    }
+      System.out.println(context.getState().toString());
 
-    @Override
-    public String onLock() {
-        player.changeState(new LockedState(player));
-        return "Locked...";
-    }
+      StopState stopState = new StopState();
+      stopState.doAction(context);
 
-    @Override
-    public String onPlay() {
-        String action = player.startPlayback();
-        player.changeState(new PlayingState(player));
-        return action;
-    }
-
-    @Override
-    public String onNext() {
-        return "Locked...";
-    }
-
-    @Override
-    public String onPrevious() {
-        return "Locked...";
-    }
+      System.out.println(context.getState().toString());
+   }
 }
-
-public abstract class State {
-    Player player;
-
-    /**
-     * Context passes itself through the state constructor. This may help a
-     * state to fetch some useful context data if needed.
-     */
-    State(Player player) {
-        this.player = player;
-    }
-
-    public abstract String onLock();
-    public abstract String onPlay();
-    public abstract String onNext();
-    public abstract String onPrevious();
-}
-
-
-import javax.swing.*;
-import java.awt.*;
-
-public class UI {
-    private Player player;
-    private static JTextField textField = new JTextField();
-
-    public UI(Player player) {
-        this.player = player;
-    }
-
-    public void init() {
-        JFrame frame = new JFrame("Test player");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JPanel context = new JPanel();
-        context.setLayout(new BoxLayout(context, BoxLayout.Y_AXIS));
-        frame.getContentPane().add(context);
-        JPanel buttons = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        context.add(textField);
-        context.add(buttons);
-
-        // Context delegates handling user's input to a state object. Naturally,
-        // the outcome will depend on what state is currently active, since all
-        // states can handle the input differently.
-        JButton play = new JButton("Play");
-        play.addActionListener(e -> textField.setText(player.getState().onPlay()));
-        JButton stop = new JButton("Stop");
-        stop.addActionListener(e -> textField.setText(player.getState().onLock()));
-        JButton next = new JButton("Next");
-        next.addActionListener(e -> textField.setText(player.getState().onNext()));
-        JButton prev = new JButton("Prev");
-        prev.addActionListener(e -> textField.setText(player.getState().onPrevious()));
-        frame.setVisible(true);
-        frame.setSize(300, 100);
-        buttons.add(play);
-        buttons.add(stop);
-        buttons.add(next);
-        buttons.add(prev);
-    }
-}
-
-
-
-
 
