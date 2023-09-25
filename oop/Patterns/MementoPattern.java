b2c45c3e5d705712a3e7997e6cbc4ec190df7589
Md Shoaib Tasrif Emon <107314630+shoaib221@users.noreply.github.com>
2023-09-25 11:11:59 +0600

    Update MementoPattern.java

--- oop/Patterns/MementoPattern.java
+++ oop/Patterns/MementoPattern.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package MementoPattern;
+// Capture and restore an object's internal state
+// tutorials point
 
-/**
- *
- * @author shoai
- */
+
 public class Main{
     public static void main(String[] args) {
         Caretaker caretaker = new Caretaker();
