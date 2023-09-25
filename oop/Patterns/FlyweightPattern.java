a226a8ff3cd3bbcfde8a0e68ab7d23d867b3965f
Md Shoaib Tasrif Emon <107314630+shoaib221@users.noreply.github.com>
2023-09-25 19:02:18 +0600

    Update FlyweightPattern.java

--- oop/Patterns/FlyweightPattern.java
+++ oop/Patterns/FlyweightPattern.java
@@ -1,114 +1,97 @@
+// https://www.tutorialspoint.com/design_pattern/flyweight_pattern.htm
 
-import java.awt.*;
-
-public class Maven_java {
-    static int CANVAS_SIZE = 500;
-    static int TREES_TO_DRAW = 1000000;
-    static int TREE_TYPES = 2;
-
-    public static void main(String[] args) {
-        Forest forest = new Forest();
-        for (int i = 0; i < Math.floor(TREES_TO_DRAW / TREE_TYPES); i++) {
-            forest.plantTree(random(0, CANVAS_SIZE), random(0, CANVAS_SIZE),
-                    "Summer Oak", Color.GREEN, "Oak texture stub");
-            forest.plantTree(random(0, CANVAS_SIZE), random(0, CANVAS_SIZE),
-                    "Autumn Oak", Color.ORANGE, "Autumn Oak texture stub");
-        }
-        forest.setSize(CANVAS_SIZE, CANVAS_SIZE);
-        forest.setVisible(true);
-
-        System.out.println(TREES_TO_DRAW + " trees drawn");
-        System.out.println("---------------------");
-        System.out.println("Memory usage:");
-        System.out.println("Tree size (8 bytes) * " + TREES_TO_DRAW);
-        System.out.println("+ TreeTypes size (~30 bytes) * " + TREE_TYPES + "");
-        System.out.println("---------------------");
-        System.out.println("Total: " + ((TREES_TO_DRAW * 8 + TREE_TYPES * 30) / 1024 / 1024) +
-                "MB (instead of " + ((TREES_TO_DRAW * 38) / 1024 / 1024) + "MB)");
-    }
-
-    private static int random(int min, int max) {
-        return min + (int) (Math.random() * ((max - min) + 1));
-    }
+interface Shape 
+{
+   void draw();
 }
 
+class Circle implements Shape 
+{
+    
+   private String color;
+   private int x;
+   private int y;
+   private int radius;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
+   public Circle(String color){
+      this.color = color;		
+   }
 
-public class Forest extends JFrame {
-    private List<Tree> trees = new ArrayList<>();
+   public void setX(int x) {
+      this.x = x;
+   }
 
-    public void plantTree(int x, int y, String name, Color color, String otherTreeData) {
-        TreeType type = TreeFactory.getTreeType(name, color, otherTreeData);
-        Tree tree = new Tree(x, y, type);
-        trees.add(tree);
-    }
+   public void setY(int y) {
+      this.y = y;
+   }
 
-    @Override
-    public void paint(Graphics graphics) {
-        for (Tree tree : trees) {
-            tree.draw(graphics);
-        }
-    }
+   public void setRadius(int radius) {
+      this.radius = radius;
+   }
+
+   @Override
+   public void draw() {
+      System.out.println("Circle: Draw() [Color : " + color + ", x : " + x + ", y :" + y + ", radius :" + radius);
+   }
 }
 
-
-import java.awt.*;
-
-public class Tree {
-    private int x;
-    private int y;
-    private TreeType type;
-
-    public Tree(int x, int y, TreeType type) {
-        this.x = x;
-        this.y = y;
-        this.type = type;
-    }
-
-    public void draw(Graphics g) {
-        type.draw(g, x, y);
-    }
-}
-
-import java.awt.*;
 import java.util.HashMap;
-import java.util.Map;
 
-public class TreeFactory {
-    static Map<String, TreeType> treeTypes = new HashMap<>();
+class ShapeFactory 
+{
 
-    public static TreeType getTreeType(String name, Color color, String otherTreeData) {
-        TreeType result = treeTypes.get(name);
-        if (result == null) {
-            result = new TreeType(name, color, otherTreeData);
-            treeTypes.put(name, result);
-        }
-        return result;
-    }
+   // Uncomment the compiler directive line and
+   // javac *.java will compile properly.
+   // @SuppressWarnings("unchecked")
+   private static final HashMap circleMap = new HashMap();
+
+   public static Shape getCircle(String color) 
+    {
+      Circle circle = (Circle)circleMap.get(color);
+
+      if(circle == null) 
+      {
+         circle = new Circle(color);
+         circleMap.put(color, circle);
+         System.out.println("Creating circle of color : " + color);
+      }
+      return circle;
+   }
+}
+
+public class FlyweightPattern
+{
+   private static final String colors[] = { "Red", "Green", "Blue", "White", "Black" };
+   public static void main(String[] args) 
+   {
+
+      for(int i=0; i < 20; ++i) 
+      {
+         Circle circle = (Circle)ShapeFactory.getCircle(getRandomColor());
+         circle.setX(getRandomX());
+         circle.setY(getRandomY());
+         circle.setRadius(100);
+         circle.draw();
+      }
+   }
+    
+   private static String getRandomColor() 
+   {
+      return colors[(int)(Math.random()*colors.length)];
+   }
+    
+   private static int getRandomX() 
+   {
+      return (int)(Math.random()*100 );
+   }
+    
+   private static int getRandomY() 
+   {
+      return (int)(Math.random()*100);
+   }
+
+    
 }
 
 
-import java.awt.*;
 
-public class TreeType {
-    private String name;
-    private Color color;
-    private String otherTreeData;
-
-    public TreeType(String name, Color color, String otherTreeData) {
-        this.name = name;
-        this.color = color;
-        this.otherTreeData = otherTreeData;
-    }
-
-    public void draw(Graphics g, int x, int y) {
-        g.setColor(Color.BLACK);
-        g.fillRect(x - 1, y, 3, 5);
-        g.setColor(color);
-        g.fillOval(x - 5, y - 10, 10, 10);
-    }
-}
