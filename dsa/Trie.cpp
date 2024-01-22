68eb997a88d90b3591cece96223e03b4e8612984
Md Shoaib Tasrif Emon <107314630+shoaib221@users.noreply.github.com>
2024-01-22 13:03:15 +0600

    Update Trie.cpp

--- dsa/Trie.cpp
+++ dsa/Trie.cpp
@@ -5,75 +5,53 @@
     n is the number of words in which to be searched for
     when trie works in O(m)
 */
-struct TrieNode
-{
-    TrieNode( char c='\0', bool isend=false )
-    {
-        val=c;
-        isEnd=isend;
-        child=vector<TrieNode*> (26);
-    }
-    char val='\0';
-    bool isEnd=false;
-    vector<TrieNode*> child;
-};
-
+ 
 struct Trie
 {
-
-    void insert_(string s)
+    struct TrieNode
     {
-        TrieNode* now=root;
-        for(int i=0; i<s.length(); i++)
+        vector<int> child;
+        int par=-1, val=-1;
+        TrieNode(int par=-1, int val=-1)
         {
-            int key=s[i]-'a';
-            if(!now->child[key])  now->child[key]=new TrieNode( s[i] ) ;
-            now= now->child[key];
+            child=vector<int> (10, -1);
+            this->par=par;
+            this->val=val;
         }
-        now->isEnd=true;
-    }
-
-    void print()
+    };
+    
+    vector<TrieNode> con;
+    Trie ()
     {
-        dfsPrint( "", root );
+        con= vector<TrieNode> (1);
     }
-
-    void delet(string x)
+ 
+    void insert( vi ara )
     {
-        TrieNode* now=root;
-        for(int i=0; i<x.length(); i++)
+        int now=0;
+        for(auto x: ara)
         {
-            int key=x[i]-'a';
-            if(now->child[key]) now=now->child[key];
-            else return;
-        }
-        now->isEnd=false;
-    }
-
-    bool search_(string x)
-    {
-        TrieNode* now=root;
-        for(int i=0; i<x.length(); i++)
-        {
-            int key=x[i]-'a';
-            if(now->child[key]) now=now->child[key];
-            else return false;
-        }
-        return now->isEnd;
-    }
-
-private:
-    TrieNode* root=new TrieNode();
-    void dfsPrint( string s, TrieNode* now )
-    {
-        if( now->isEnd ) cout<<s<<N;
-        for(int i=0; i<26; i++)
-        {
-            if( now->child[i] )
+            if( con[now].child[x]==-1 )
             {
-                string y=s+(char) ( 'a'+i );
-                dfsPrint(y,now->child[i]);
+                con[now].child[x]=con.size();
+                TrieNode tn( now, x );
+                con.push_back( tn );
             }
+            now=con[now].child[x];
         }
+    }
+ 
+    int check( vi ara )
+    {
+        int now=0, ans=0;
+        for(auto x: ara)
+        {
+            //cout<<x<<D;
+            if( con[now].child[x]==-1 ) break;
+            ans++;
+            now=con[now].child[x];
+        }
+        return ans;
     }
 };
+
