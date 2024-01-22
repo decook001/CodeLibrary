30cf1551609c6c49dd5dfd92f94b1612f8ff0bc7
Md Shoaib Tasrif Emon <107314630+shoaib221@users.noreply.github.com>
2024-01-22 13:59:36 +0600

    Update Trie.cpp

--- dsa/Trie.cpp
+++ dsa/Trie.cpp
@@ -5,16 +5,15 @@
     n is the number of words in which to be searched for
     when trie works in O(m)
 */
- 
 struct Trie
 {
     struct TrieNode
     {
         vector<int> child;
-        int par=-1, val=-1;
-        TrieNode(int par=-1, int val=-1)
+        int par, val;
+        TrieNode(int par=-1, int val=0)
         {
-            child=vector<int> (10, -1);
+            child=vector<int> (28, -1);
             this->par=par;
             this->val=val;
         }
@@ -26,7 +25,7 @@ struct Trie
         con= vector<TrieNode> (1);
     }
  
-    void insert( vi ara )
+    void insert( VIN ara )
     {
         int now=0;
         for(auto x: ara)
@@ -34,24 +33,42 @@ struct Trie
             if( con[now].child[x]==-1 )
             {
                 con[now].child[x]=con.size();
-                TrieNode tn( now, x );
+                TrieNode tn( now, 0 );
                 con.push_back( tn );
             }
             now=con[now].child[x];
+            con[now].val++;
+            //cout<< now <<S<< con[now].val <<E;
         }
     }
  
-    int check( vi ara )
+    ll qry( VIN& ara, int n, ll tot )
     {
-        int now=0, ans=0;
+        ll eij=ara.size();
+        ll ans= eij*n+tot , sum=0, pre=n, len=0;
+        int now=0;
+        VIN aha(1,n);
         for(auto x: ara)
         {
             //cout<<x<<D;
             if( con[now].child[x]==-1 ) break;
-            ans++;
             now=con[now].child[x];
+            len++;
+            aha.push_back( con[now].val );
         }
+        //cout<< ans <<D;
+
+        for( int i=1;i< aha.size();i++  )
+        {
+            //cout<< aha[i] <<SS;
+            ll var1=aha[i];
+            if( i<aha.size()-1 )
+            {
+                var1-=aha[i+1];
+            }
+            ans-=var1*i*2;
+        }
+        //cout<<F;
         return ans;
     }
 };
-
